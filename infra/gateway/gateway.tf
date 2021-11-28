094570bf54195da4e25b1931cb8aa44d798750cb
ericksoen <michael.ericksen@gmail.com>
2021-11-27 22:26:29 -0600

    infra: resolve s3 bucket security issues

--- infra/gateway/gateway.tf
+++ infra/gateway/gateway.tf
@@ -147,4 +147,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
   block_public_acls   = true
   block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls = true
 }
