14eb25e908a04875e2c64f195b3a03813d28a095
ericksoen <michael.ericksen@gmail.com>
2021-11-27 22:33:47 -0600

    infra: enable server side encryption for bucket

--- infra/gateway/gateway.tf
+++ infra/gateway/gateway.tf
@@ -140,13 +140,21 @@ resource "aws_s3_bucket" "b" {
     target_bucket = "bucket-logging-demo"
     target_prefix = "logs/"
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.b.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
   restrict_public_buckets = true
-  ignore_public_acls = true
+  ignore_public_acls      = true
 }
