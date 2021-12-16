2eed16f01c39ca13fbf4747ba815078942e37af5
ericksoen <michael.ericksen@gmail.com>
2021-12-16 12:37:24 -0600

    resolve invalid id format for initial request to dotnet app (#30)
    closes #29

--- examples/dotnet/app/Program.cs
+++ examples/dotnet/app/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,15 @@ namespace app
     {
         public static void Main(string[] args)
         {
+            // Initialize Activity Id format to use W3C before building application
+            // since default dotnet 3.1 Activity is hierarchical.
+            
+            // Eventually the OpenTelemetry SDK changes the format to W3C, but the 
+            // first application request is already in flight prior to that change.
+
+            // See: https://github.com/open-telemetry/opentelemetry-dotnet/issues/2746#issuecomment-995357477
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+            Activity.ForceDefaultIdFormat = true;            
             CreateHostBuilder(args).Build().Run();
         }
 
