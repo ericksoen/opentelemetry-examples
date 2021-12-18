dbf4d1afe79e16f652619b8b0b7c621c8d6d9e0f
ericksoen <michael.ericksen@gmail.com>
2021-12-18 15:16:40 -0600

    use sdk extension to detect ec2 resource attributes
    closes #32

--- src/ec2/app.py
+++ src/ec2/app.py
@@ -12,36 +12,40 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.extension.aws.resource.ec2 import (
+    AwsEc2ResourceDetector,
+)
+from opentelemetry.sdk.resources import get_aggregated_resources
 
 import boto3
 
 MIN_LATENCY_MS = 600
 MAX_LATENCY_MS = 1500
-raw = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
-
-instance_data = raw.json()
-client = boto3.client('ssm', region_name=instance_data["region"])
 
 trace.set_tracer_provider(
     TracerProvider(
-        resource=Resource.create(
-            {
-                SERVICE_NAME: "ec2-service",
-                "instance.availabilityZone": instance_data["availabilityZone"],
-                "instance.architecture": instance_data["architecture"],
-                "instance.instanceId": instance_data["instanceId"],
-                "instance.imageId": instance_data["imageId"],
-                "instance.instanceType": instance_data["instanceType"],
-                "instance.region": instance_data["region"],
-            }
-        )
+        resource=get_aggregated_resources(
+            [
+                AwsEc2ResourceDetector(),
+            ],
+            Resource.create({
+                SERVICE_NAME: "ec2-service"
+            })
+        ),
     )
 )
 
+# TODO: this still feels like unnecessary overhead to identify the region
+# in order to essentially acquire an environment variable.
+raw = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+instance_data = raw.json()
+region = instance_data["region"]
+client = boto3.client('ssm', region_name=region)
 lambda_target_response = client.get_parameter(Name="lambda-target-url")
 lambda_target = lambda_target_response['Parameter']['Value']
 otlp_target = os.getenv('OTLP_TARGET', "http://127.0.0.1:4317")
-print(f"The OLTP target = {otlp_target}")
+
+
 otlp_exporter = OTLPSpanExporter(endpoint=otlp_target, insecure=True)
 span_processor = BatchSpanProcessor(otlp_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
