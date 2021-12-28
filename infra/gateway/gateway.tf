23f641ab7408895303f546815a2d72e2a9769795
ericksoen <michael.ericksen@gmail.com>
2021-12-27 19:43:06 -0600

    chore: remove amazon/aws-otel-collector version pin

--- infra/gateway/gateway.tf
+++ infra/gateway/gateway.tf
@@ -60,15 +60,6 @@ resource "aws_ecs_service" "gateway" {
   ]
 }
 
-locals {
-
-  # As of 11/20/21, versions subsequent to v0.13.0 do not correctly
-  # expand environment variables and instead treat them as string literals
-  # Pin to the last known working version until the issue is resolved
-  # See: https://github.com/aws-observability/aws-otel-collector/issues/748
-  aws_otel_collector_image_version = "v0.13.0"
-
-}
 resource "aws_ecs_task_definition" "gateway" {
   family                   = var.resource_prefix
   network_mode             = "awsvpc"
@@ -82,7 +73,7 @@ resource "aws_ecs_task_definition" "gateway" {
   container_definitions = jsonencode([
     {
       name      = "${var.resource_prefix}"
-      image     = "amazon/aws-otel-collector:${local.aws_otel_collector_image_version}"
+      image     = "amazon/aws-otel-collector"
       essential = true
       logConfiguration = {
         logDriver = "awslogs"
