6420a4d3a1622258135597a27b2cc380b4a5b5b1
ericksoen <michael.ericksen@gmail.com>
2022-01-09 21:12:58 -0600

    add boto3 instrumentation to lambda function

--- src/lambda-python/main.py
+++ src/lambda-python/main.py
@@ -1,20 +1,25 @@
 import time
 import json
 from opentelemetry import trace
+import boto3
 
+client = boto3.client('s3')
 tracer = trace.get_tracer(__name__)
 
 def handler(event, context):
 
-    with tracer.start_as_current_span("example-request") as f:
-        message = "hello world"
-        f.set_attribute("message", message)
+
+    with tracer.start_as_current_span("list-s3-buckets") as f:
+
+        response = client.list_buckets()
+        bucket_count = len(response['Buckets'])
+        f.set_attribute("s3.bucket_count", bucket_count)
         time.sleep(.35)
         return {
             "statusCode": 200,
             "isBase64Encoded": False,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps({"message": message}),
+            "body": json.dumps({"s3.bucket_count": bucket_count}),
         }
 
 if __name__ == "__main__":
