eaeabe50726e43a10daeec059e8dbab06c809388
ericksoen <michael.ericksen@gmail.com>
2022-01-10 08:31:15 -0600

    Add boto3 instrumentation (#54)
    * add boto3 instrumentation to lambda function
    * add botocore instrumentation to ec2 endpoint
    * add botocore instrumentation to ecs endpoint

--- infra/modules/lambda/iam.tf
+++ infra/modules/lambda/iam.tf
@@ -28,6 +28,11 @@ resource "aws_iam_role_policy_attachment" "ro" {
   policy_arn = "arn:aws:iam::aws:policy/AWSLambda_ReadOnlyAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "s3_ro" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "xray" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
